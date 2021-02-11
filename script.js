c8556edc55ea71f7240d72e9cc03599e8c0343a1
zoeadams256 <78935392+zoeadams256@users.noreply.github.com>
2021-02-11 17:01:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var name= prompt("What is your name?");
+var color = prompt("What is your favorite color");
+documentbody.querySelector(".writer"). innerHTML=name+" is my name! "+color+" is my favorte color!";
